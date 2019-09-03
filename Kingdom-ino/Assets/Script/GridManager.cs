35a1943432eb93178a3418356b59b25c6561d709
Jeremy Morel <jeremy.morel.jm@gmail.com>
2019-09-03 23:02:23 +0200

    start

---
+++ Kingdom-ino/Assets/Script/GridManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    private Grid m_Grid;
+
+    private void Awake()
+    {
+        m_Grid = this.GetComponent<Grid>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hitInfo))
+            {
+                GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position =  m_Grid.GetCellCenterWorld(m_Grid.WorldToCell(hitInfo.point));
+            }
+        }
+    }
+}
