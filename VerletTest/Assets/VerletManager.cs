bbc36d54d9d4c8d550098985b3a41660628ac2ec
Jeremy Morel <jeremy.morel.jm@gmail.com>
2020-03-28 21:14:30 +0100

    wind OK

--- VerletTest/Assets/VerletManager.cs
+++ VerletTest/Assets/VerletManager.cs
@@ -4,116 +4,114 @@ using UnityEngine;
 
 public class VerletManager : MonoBehaviour
 {
-
-    private LineRenderer lineRenderer;
     public Mesh msh;
     Vector3[] vertices;
+    Vector3[] verticesOld;
     int[] triangles;
-    public List<List<VerletPoint>> verletPointMatrix = new List<List<VerletPoint>>();
+    public List<int> pinnedVerticesIndex = new List<int>();
     public List<VerletLink> verletLink = new List<VerletLink>();
     public float LinkLength = 0.25f;
-    public int verletLength = 10;
     public int verletHeight = 10;
-    private float lineWidth = 0.1f;
+    public int verletLength = 10;
     public int iterationCount = 50;
-    public Vector3 mousePosition;
 
-    // Start is called before the first frame update
+    public GameObject[] ventilateurs;
+    public float ventilateurStrenght = 2.0f;
+    public float ventilateurDistanceMax = 1.0f;
+
+
+    // Use this for initialization
     void Start()
     {
-        this.lineRenderer = this.GetComponent<LineRenderer>();
+        ventilateurs = GameObject.FindGameObjectsWithTag("Ventilateur");
+
+        InitVerticeAndLink();
+    }
+
+    void InitVerticeAndLink()
+    {
         Vector3 verletStartPoint = this.transform.position;
 
-        for (int i = 0; i < verletHeight; i++)
+        vertices = new Vector3[(verletLength) * (verletHeight)];
+        verticesOld = new Vector3[(verletLength) * (verletHeight)];
+        int idx = 0;
+        for (int i = 0; i < verletLength; i++)
         {
-            List<VerletPoint> verletLengthList = new List<VerletPoint>();
-            for (int j = 0; j < verletLength; j++)
+            for (int j = 0; j < verletHeight; j++)
             {
-                verletLengthList.Add(new VerletPoint(verletStartPoint));
+                vertices[idx] = verletStartPoint;
+                verticesOld[idx] = verletStartPoint;
+                idx++;
                 verletStartPoint.y -= LinkLength;
             }
             verletStartPoint.x += LinkLength;
             verletStartPoint.y = this.transform.position.y;
-            verletPointMatrix.Add(verletLengthList);
         }
+
         //creation du maillage
 
-        for (int i = 0; i < verletHeight; i++)
+        for (int i = 0; i < verletLength; i++)
         {
-            for (int j = 0; j < verletLength; j++)
+            for (int j = 0; j < verletHeight; j++)
             {
                 //linkDown
-                if (j < verletLength - 1)
+                if (j < verletHeight - 1)
                 {
                     verletLink.Add(new VerletLink(new Vector2(i, j), new Vector2(i, j + 1)));
                 }
 
                 //linkRight
 
-                if (i < verletHeight - 1)
+                if (i < verletLength - 1)
                 {
                     verletLink.Add(new VerletLink(new Vector2(i, j), new Vector2(i + 1, j)));
                 }
             }
         }
 
+        //
+        /*for(int i = 0; i< verletLength;i++)
+        {
+            pinnedVerticesIndex.Add(i * verletHeight);
+        }*/
+        pinnedVerticesIndex.Add(0);
+        pinnedVerticesIndex.Add((verletLength - 1) * verletHeight);
+
+
         // Create the mesh
+        //GameObject meshGo = new GameObject();
         msh = new Mesh();
 
         // Set up game object with mesh;
+        //meshGo.AddComponent(typeof(MeshRenderer));
         MeshFilter filter = gameObject.AddComponent(typeof(MeshFilter)) as MeshFilter;
         filter.mesh = msh;
 
-        //CreateShape();
-
-        //UpdateMesh();
+        RecomputeTriangle();
     }
 
-    void CreateShape()
+    void RecomputeTriangle()
     {
-        /*vertices = new Vector3[]
-        {
-            new Vector3 (0,0,0),
-            new Vector3 (0,0,1),
-            new Vector3 (1,0,0),
-            new Vector3 (1,0,1)
-        };
-
-        triangles = new int[]
-        {
-            0,1,2, 1,3,2
-        };*/
-        vertices = new Vector3[(verletHeight) * (verletLength)];
-        for(int i =0, z = 0; z < verletLength; z++)
-        {
-            for (int x = 0; x < verletHeight; x++)
-            {
-                vertices[i] = new Vector3(verletPointMatrix[z][x].posNow.x, verletPointMatrix[z][x].posNow.y, 0); //verletPointMatrix[z][x].posNow.x,0, verletPointMatrix[z][x].posNow.y);
-                i++;
-            }
-        }
-
-        
-        triangles = new int[(verletHeight-1) *(verletLength -1 )*6];
+        triangles = new int[(verletLength - 1) * (verletHeight - 1) * 6];
         int vert = 0;
         int tris = 0;
 
-        for (int z = 0; z < verletHeight -1; z++)
+        for (int z = 0; z < verletLength - 1; z++)
         {
-            for (int x = 0; x < verletLength-1; x++)
+            for (int x = 0; x < verletHeight - 1; x++)
             {
 
                 triangles[tris] = vert;
-                triangles[tris + 1] = vert + verletLength + 1;
+                triangles[tris + 1] = vert + verletHeight + 1;
                 triangles[tris + 2] = vert + 1;
 
                 triangles[tris + 3] = vert;
-                triangles[tris + 4] = vert + verletLength;
-                triangles[tris + 5] = vert + verletLength + 1;
+                triangles[tris + 4] = vert + verletHeight;
+                triangles[tris + 5] = vert + verletHeight + 1;
 
                 vert++;
                 tris += 6;
-                
+
             }
             vert++;
         }
@@ -132,42 +130,59 @@ public class VerletManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            this.DrawRope(verletPointMatrix[0]);
-        CreateShape();
-
         UpdateMesh();
+       /* if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newVentilo = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            newVentilo.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            newVentilo.tag = "Ventilateur";
+            ventilateurs = GameObject.FindGameObjectsWithTag("Ventilateur");
+        }*/
     }
 
     private void FixedUpdate()
     {
-            this.Simulate(verletPointMatrix, verletLink);
+        this.Simulate(verletLink);
     }
 
-    private void Simulate(List<List<VerletPoint>> verletPointMatrix, List<VerletLink> links)
+    private void Simulate( List<VerletLink> links)
     {
         // SIMULATION
-        Vector2 forceGravity = new Vector2(0f, -1.5f);
 
-        for (int i = 0; i < verletHeight; i++)
+        Vector3 forceGravity = new Vector3(0f, -1.5f,0f);
+
+
+        for (int i = 0; i < verletLength; i++)
         {
-            for (int j = 0; j < verletLength; j++)
+            for (int j = 0; j < verletHeight; j++)
             {
-                VerletPoint firstSegment = verletPointMatrix[i][j];
-                Vector2 velocity = firstSegment.posNow - firstSegment.posOld;
-                firstSegment.posOld = firstSegment.posNow;
-                firstSegment.posNow += velocity;
-                firstSegment.posNow += forceGravity * Time.fixedDeltaTime;
-                verletPointMatrix[i][j] = firstSegment;
+                int index = i * verletHeight + j;
+                if (pinnedVerticesIndex.Contains(index))
+                {
+                    continue;
+                }
+
+                Vector3 velocity = vertices[index] - verticesOld[index];
+                verticesOld[index] = vertices[i * verletHeight + j];
+                vertices[index] += velocity;
+                vertices[index] += forceGravity * Time.fixedDeltaTime;
+
+
+                for(int v = 0; v < ventilateurs.Length; v++)
+                {
+                    float dist = (vertices[index] - ventilateurs[v].transform.position).magnitude;
+                    if(dist < ventilateurDistanceMax)
+                    {
+                        float factor = (ventilateurDistanceMax - dist) / ventilateurDistanceMax;
+                        Vector3 dir = (vertices[index] - ventilateurs[v].transform.position).normalized;
+                        vertices[index] += factor * ventilateurStrenght * dir * Time.fixedDeltaTime * Mathf.Sin(6.0f*3.141592654f * Time.fixedDeltaTime);
+                    }
+                }
             }
         }
 
-        for (int i = 0; i < verletHeight; i++)
-        {
-            verletPointMatrix[i][0] = new VerletPoint( new Vector2(this.transform.position.x + i * LinkLength, this.transform.position.y));
-        }
-
-            //CONSTRAINTS
-            for (int i = 0; i < iterationCount; i++)
+        //CONSTRAINTS
+        for (int i = 0; i < iterationCount; i++)
         {
             this.ApplyConstraint(links);
         }
@@ -175,93 +190,82 @@ public class VerletManager : MonoBehaviour
 
     private void ApplyConstraint(List<VerletLink> links)
     {
-        //Constrant to Mouse
-       // VerletPoint firstSegment = ropeSegments[0];
-        //firstSegment.posNow = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //ropeSegments[0] = firstSegment;
 
         for (int i = 0; i < links.Count; i++)
         {
-            VerletPoint firstSeg = verletPointMatrix[(int)verletLink[i].pointPosition.x][(int)verletLink[i].pointPosition.y];
-            VerletPoint secondSeg = verletPointMatrix[(int)verletLink[i].point2Position.x][(int)verletLink[i].point2Position.y];
+            int firstPointIndex = (int)verletLink[i].pointPosition.x * verletHeight + (int)verletLink[i].pointPosition.y;
+            int secondPointIndex = (int)verletLink[i].point2Position.x * verletHeight + (int)verletLink[i].point2Position.y;
 
-            float dist = (firstSeg.posNow - secondSeg.posNow).magnitude;
+            Vector3 firstSeg = vertices[firstPointIndex];
+            Vector3 secondSeg = vertices[secondPointIndex];
+
+            float dist = (firstSeg - secondSeg).magnitude;
             float error = Mathf.Abs(dist - this.LinkLength);
-            Vector2 changeDir = Vector2.zero;
+            Vector3 changeDir = Vector2.zero;
 
             if (dist > LinkLength)
             {
-                changeDir = (firstSeg.posNow - secondSeg.posNow).normalized;
+                changeDir = (firstSeg - secondSeg).normalized;
             }
             else if (dist < LinkLength)
             {
-                changeDir = (secondSeg.posNow - firstSeg.posNow).normalized;
+                changeDir = (secondSeg - firstSeg).normalized;
             }
 
-            Vector2 changeAmount = changeDir * error;
-            if (i != 0)
+            Vector3 changeAmount = changeDir * error;
+
+            if (pinnedVerticesIndex.Contains(secondPointIndex))
             {
-                firstSeg.posNow -= changeAmount * 0.5f;
-                verletPointMatrix[(int)verletLink[i].pointPosition.x][(int)verletLink[i].pointPosition.y] = firstSeg;
-                secondSeg.posNow += changeAmount * 0.5f;
-                verletPointMatrix[(int)verletLink[i].point2Position.x][(int)verletLink[i].point2Position.y] = secondSeg;
+                firstSeg -= changeAmount;
+                vertices[firstPointIndex] = firstSeg;
+            }
+            else if (pinnedVerticesIndex.Contains(firstPointIndex))
+            {
+                secondSeg += changeAmount;
+                vertices[secondPointIndex] = secondSeg;
             }
             else
             {
-                secondSeg.posNow += changeAmount;
-                verletPointMatrix[(int)verletLink[i].point2Position.x][(int)verletLink[i].point2Position.y] = secondSeg;
+                firstSeg -= changeAmount * 0.5f;
+                vertices[firstPointIndex] = firstSeg;
+                secondSeg += changeAmount * 0.5f;
+                vertices[secondPointIndex] = secondSeg;
             }
         }
-    }
-
-    private void DrawRope(List<VerletPoint> ropeSegments)
-    {
-       /* float lineWidth = this.lineWidth;
-        lineRenderer.startWidth = lineWidth;
-        lineRenderer.endWidth = lineWidth;
-
-        Vector3[] ropePositions = new Vector3[this.verletLength];
-        for (int i = 0; i < this.verletLength; i++)
-        {
-            ropePositions[i] = ropeSegments[i].posNow;
-            Gizmos.color = Color.yellow;
-            
-        }
-
-        lineRenderer.positionCount = ropePositions.Length;
-        lineRenderer.SetPositions(ropePositions);*/
     }
 
     void OnDrawGizmosSelected()
     {
-        if (verletPointMatrix.Count != 0)
+        if (vertices.Length != 0)
         {
-            for (int i = 0; i < verletHeight; i++)
+            for (int i = 0; i < verletLength; i++)
             {
-                for (int j = 0; j < this.verletLength; j++)
+                for (int j = 0; j < this.verletHeight; j++)
                 {
-                    // Draw a yellow sphere at the transform's position
                     Gizmos.color = Color.red;
-                    Gizmos.DrawSphere(verletPointMatrix[i][j].posNow, 0.1f);
+                    Gizmos.DrawSphere(vertices[i * verletHeight + j], 0.1f);
                 }
             }
 
+            Gizmos.color = Color.blue;
+            Vector3 verletStartPoint = this.transform.position;
+            for (int i = 0; i < verletLength; i++)
+            {
+                for (int j = 0; j < verletHeight; j++)
+                {
+                    Gizmos.DrawSphere(verletStartPoint,0.05f);
+                    verletStartPoint.y -= LinkLength;
+                }
+                verletStartPoint.x += LinkLength;
+                verletStartPoint.y = this.transform.position.y;
+            }
+
+
+            Gizmos.color = Color.white;
             for (int i = 0; i < verletLink.Count; i++)
             {
-                Debug.DrawLine(verletPointMatrix[(int)verletLink[i].pointPosition.x][(int)verletLink[i].pointPosition.y].posNow, verletPointMatrix[(int)verletLink[i].point2Position.x][(int)verletLink[i].point2Position.y].posNow, Color.white);
+                Debug.DrawLine(vertices[(int)verletLink[i].pointPosition.x * verletHeight + (int)verletLink[i].pointPosition.y], vertices[(int)verletLink[i].point2Position.x * verletHeight + (int)verletLink[i].point2Position.y], Color.white);
             }
-        }
-    }
-
-    public struct VerletPoint
-    {
-        public Vector2 posNow;
-        public Vector2 posOld;
-
-        public VerletPoint(Vector2 pos)
-        {
-            this.posNow = pos;
-            this.posOld = pos;
         }
     }
 
