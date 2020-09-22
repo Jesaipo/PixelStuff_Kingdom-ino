92df3e8bf35b661ff805c9cda8e084d139e0d6a9
Jeremy Morel <jeremy.morel.jm@gmail.com>
2020-09-22 23:26:18 +0200

    add poly asset and start positionning

--- Kingdom-ino/Assets/Script/GridManager.cs
+++ Kingdom-ino/Assets/Script/GridManager.cs
@@ -9,6 +9,13 @@ public class GridManager : MonoBehaviour
     static Vector3Int LEFT = new Vector3Int(-1, 0, 0);
     static Vector3Int RIGHT = new Vector3Int(1, 0, 0);
 
+    enum ERotationVector
+    {
+        Left,
+        Right,
+        None
+    };
+
     private Grid m_Grid;
 
     public List<Cell> m_CellList = new List<Cell>();
@@ -17,6 +24,8 @@ public class GridManager : MonoBehaviour
     public GameObject m_StartCell;
 
     public List<GameObject> m_SquarePrefab = new List<GameObject>();
+
+    private bool m_IsNewCellValid = false;
 
     private void Awake()
     {
@@ -36,37 +45,70 @@ public class GridManager : MonoBehaviour
         if (m_NewCellList.Count > 1)
         {
             Vector3Int CellMoveVector = Vector3Int.zero;
-            if (Input.GetKeyDown(KeyCode.Z))
+            ERotationVector rotation = ERotationVector.None;
+            if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.UpArrow))
             {
                 CellMoveVector += UP;
             }
-            if (Input.GetKeyDown(KeyCode.S))
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
             {
                 CellMoveVector += DOWN;
             }
-            if (Input.GetKeyDown(KeyCode.Q))
+            if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.LeftArrow))
             {
                 CellMoveVector += LEFT;
             }
-            if (Input.GetKeyDown(KeyCode.D))
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
             {
                 CellMoveVector += RIGHT;
             }
 
-            if (CellMoveVector != Vector3Int.zero)
+            if(Input.GetKeyDown(KeyCode.A))
             {
-                
+                rotation = ERotationVector.Left;
+            }
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                rotation = ERotationVector.Right;
+            }
+
+            if (CellMoveVector != Vector3Int.zero || rotation != ERotationVector.None)
+            {
+                m_IsNewCellValid = false;
+                bool isOnTop = false;
                 foreach (Cell targetCell in m_NewCellList)
                 {
                     targetCell.m_GridPosition.y = 0;
                     Vector3Int newGridPosition = targetCell.m_GridPosition + CellMoveVector;
-                    bool isValid = false;
-                    bool isOnTop = false;
+
+                    if(rotation != ERotationVector.None)
+                    {
+                        Cell pivotCell = m_NewCellList[0];
+                        Vector3Int pivotPosition = newGridPosition - pivotCell.m_GridPosition;
+                        Vector3Int rotatedPivotPosition = Vector3Int.zero;
+                        int rotationMultiplier = 0;
+                        if (rotation == ERotationVector.Right)
+                        {
+                            //rotate
+                            rotatedPivotPosition = new Vector3Int(pivotPosition.z, pivotPosition.y, -pivotPosition.x);
+                            rotationMultiplier = 1;
+                        }
+                        else if(rotation == ERotationVector.Left)
+                        {
+                            //rotate
+                            rotatedPivotPosition = new Vector3Int(-pivotPosition.z, pivotPosition.y, pivotPosition.x);
+                            rotationMultiplier = -1;
+                        }
+
+                        newGridPosition = pivotCell.m_GridPosition + rotatedPivotPosition;
+                        targetCell.m_Object.transform.Rotate(Vector3.up, rotationMultiplier * 90);
+                    }
+
                     foreach (Cell existingCell in m_CellList)
                     {
                         if (existingCell.m_GridPosition == newGridPosition)
                         {
-                            isValid = false;
+                            m_IsNewCellValid = false;
                             isOnTop = true;
                             break;
                         }
@@ -77,7 +119,7 @@ public class GridManager : MonoBehaviour
                         {
                             if(targetCell.m_Object.CompareTag(existingCell.m_Object.tag) || existingCell.m_Object.CompareTag(m_StartCell.tag))
                             {
-                                isValid = true;
+                                m_IsNewCellValid = true;
                             }
                         }
                     }
@@ -90,40 +132,48 @@ public class GridManager : MonoBehaviour
                         newGridPosition.y = 0;
                     }
 
-                    if (isValid)
+                    targetCell.m_GridPosition = newGridPosition;
+                    targetCell.m_Object.transform.position = m_Grid.CellToWorld(targetCell.m_GridPosition);
+                }
+
+                foreach (Cell targetCell in m_NewCellList)
+                {
+                    if (isOnTop || !m_IsNewCellValid)
                     {
-                        targetCell.m_Object.transform.localScale = new Vector3(1f, 1f, 1f);
+                        targetCell.m_Object.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
                     }
                     else
                     {
-                        targetCell.m_Object.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                        targetCell.m_Object.transform.localScale = new Vector3(1f, 1f, 1f);
                     }
-
-                    targetCell.m_GridPosition = newGridPosition;
-                    targetCell.m_Object.transform.position = m_Grid.CellToWorld(targetCell.m_GridPosition);
                 }
             }
         }
 
         if (Input.GetKeyDown(KeyCode.N))
         {
-            Debug.Log("New");
-            Vector3Int CellPosition = new Vector3Int(0, 0, 0);
-            int rand = Random.Range(0, m_SquarePrefab.Count);
-            m_NewCellList.Add(new Cell(m_SquarePrefab[rand], m_Grid.CellToWorld(CellPosition), CellPosition));
-            rand = Random.Range(0, m_SquarePrefab.Count);
-            CellPosition.x += 1;
-            m_NewCellList.Add(new Cell(m_SquarePrefab[rand], m_Grid.CellToWorld(CellPosition), CellPosition));
+            if (m_NewCellList.Count == 0)
+            {
+                Vector3Int CellPosition = new Vector3Int(0, 0, 0);
+                int rand = Random.Range(0, m_SquarePrefab.Count);
+                m_NewCellList.Add(new Cell(m_SquarePrefab[rand], m_Grid.CellToWorld(CellPosition), CellPosition));
+                rand = Random.Range(0, m_SquarePrefab.Count);
+                CellPosition.x += 1;
+                m_NewCellList.Add(new Cell(m_SquarePrefab[rand], m_Grid.CellToWorld(CellPosition), CellPosition));
+            }
 
         }
         if (Input.GetKeyDown(KeyCode.B))
         {
-            foreach (Cell targetCell in m_NewCellList)
+            if (m_IsNewCellValid)
             {
-                m_CellList.Add(targetCell);
-            }
+                foreach (Cell targetCell in m_NewCellList)
+                {
+                    m_CellList.Add(targetCell);
+                }
 
-            m_NewCellList.Clear();
+                m_NewCellList.Clear();
+            }
         }
 
         if (Input.GetMouseButtonDown(0))
